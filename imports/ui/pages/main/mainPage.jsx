34e77119983783fd69c12545dd86b23fd13cff0a
Christian Böndgen <c.boendgen@gmail.com>
2026-02-16 11:28:32 +0100

    add main page translations

--- imports/ui/pages/main/mainPage.jsx
+++ imports/ui/pages/main/mainPage.jsx
@@ -1,29 +1,25 @@
 import {Button} from "@/components/ui/button";
 import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
 import React from "react";
+import {useTranslation} from "react-i18next";
 import {useNavigate} from "react-router-dom";
 
 export function MainPage() {
   const navigate = useNavigate();
+  const {t} = useTranslation();
 
   return (
     <div className="w-full flex justify-center items-center">
       <Card className="max-w-150">
         <CardHeader>
-          <CardTitle className="text-center">Welcome to the survey!</CardTitle>
-          <CardDescription>
-            Thank you for checking it out. This survey will take about 15 minutes of your time.
-          </CardDescription>
+          <CardTitle className="text-center">{t("MainPage.title")}</CardTitle>
+          <CardDescription>{t("MainPage.description")}</CardDescription>
         </CardHeader>
         <CardContent>
-          <p>
-            This Survey is about the similarity of singing voices. You will hear three short audio clips (A, B and X). X is
-            the target voice and A and B are to be compared to it. Choose the audio clip in which the voice is most similar
-            with the one from the target audio clip X.
-          </p>
+          <p>{t("MainPage.content")}</p>
         </CardContent>
         <CardFooter>
-          <Button onClick={() => navigate("/survey")}>To the survey</Button>
+          <Button onClick={() => navigate("/survey")}>{t("MainPage.startSurvey")}</Button>
         </CardFooter>
       </Card>
     </div>
