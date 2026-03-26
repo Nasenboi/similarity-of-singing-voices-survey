80e69776c96a4c54436870ae0daead045f9420e5
Christian Böndgen <c.boendgen@gmail.com>
2026-03-26 12:53:33 +0100

    add className to typography components

--- imports/ui/customComponents/Typography.jsx
+++ imports/ui/customComponents/Typography.jsx
@@ -2,38 +2,42 @@ import React from "react";
 
 // inspired by https://ui.shadcn.com/docs/components/radix/typography
 
-export function H1({children}) {
-  return <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">{children}</h1>;
+export function H1({children, className}) {
+  return (
+    <h1 className={"scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance " + className}>
+      {children}
+    </h1>
+  );
 }
 
-export function H2({children}) {
-  return <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">{children}</h2>;
+export function H2({children, className}) {
+  return <h2 className={"scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0 " + className}>{children}</h2>;
 }
 
-export function H3({children}) {
-  return <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">{children}</h3>;
+export function H3({children, className}) {
+  return <h3 className={"scroll-m-20 text-2xl font-semibold tracking-tight " + className}>{children}</h3>;
 }
 
-export function H4({children}) {
-  return <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">{children}</h4>;
+export function H4({children, className}) {
+  return <h4 className={"scroll-m-20 text-xl font-semibold tracking-tight " + className}>{children}</h4>;
 }
 
-export function P({children}) {
-  return <p className="leading-7 not-first:mt-6">{children}</p>;
+export function P({children, className}) {
+  return <p className={"leading-7 not-first:mt-6 " + className}>{children}</p>;
 }
 
-export function Lead({children}) {
-  return <p className="text-xl text-muted-foreground">{children}</p>;
+export function Lead({children, className}) {
+  return <p className={"text-xl text-muted-foreground " + className}>{children}</p>;
 }
 
-export function Large({children}) {
-  return <div className="text-lg font-semibold">{children}</div>;
+export function Large({children, className}) {
+  return <div className={"text-lg font-semibold " + className}>{children}</div>;
 }
 
-export function Small({children}) {
-  return <small className="text-sm leading-none font-medium">{children}</small>;
+export function Small({children, className}) {
+  return <small className={"text-sm leading-none font-medium " + className}>{children}</small>;
 }
 
-export function Muted({children}) {
-  return <p className="text-sm text-muted-foreground">{children}</p>;
+export function Muted({children, className}) {
+  return <p className={"text-sm text-muted-foreground " + className}>{children}</p>;
 }
