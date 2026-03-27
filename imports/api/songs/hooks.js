3d56cad875416a4ecbb6e64e9dd6a81edd89ceda
Christian Böndgen <c.boendgen@gmail.com>
2026-03-27 11:53:47 +0100

    rm unused code

--- imports/api/songs/hooks.js
+++ imports/api/songs/hooks.js
@@ -47,29 +47,3 @@ export const useSongsAll = ({participantID, fields}) =>
       isLoading: !subscriptionHandle.ready(),
     };
   }, [Meteor.userId(), participantID]);
-
-/*
-export const useSongsParticipant = (participantID) =>
-  useTracker(() => {
-    if (!participantID) return {songs: null, isLoading: false};
-    const subscriptionHandle = Meteor.subscribe("songs.participant", participantID);
-    const songs = Songs.find().fetch();
-
-    return {
-      songs,
-      isLoading: !subscriptionHandle.ready(),
-    };
-  }, [participantID]);
-
-export const useSongsSurveyQuestion = (questionnaireID, questionNumber) =>
-  useTracker(() => {
-    if (questionnaireID == null || questionNumber == null) return {songs: null, isLoading: false};
-    const subscriptionHandle = Meteor.subscribe("songs.surveyQuestion", questionnaireID, questionNumber);
-    const songs = Songs.find().fetch();
-
-    return {
-      songs,
-      isLoading: !subscriptionHandle.ready(),
-    };
-  }, [questionnaireID, questionNumber]);
-*/
