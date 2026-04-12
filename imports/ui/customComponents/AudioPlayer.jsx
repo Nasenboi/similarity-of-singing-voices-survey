c84c1ed6484687d4a1747475d6b8e5b2e567a6cb
Christian Böndgen <c.boendgen@gmail.com>
2026-04-12 12:37:38 +0200

    add license information
    issue => #20

--- imports/ui/customComponents/AudioPlayer.jsx
+++ imports/ui/customComponents/AudioPlayer.jsx
@@ -1,14 +1,43 @@
 import {Button} from "@/components/ui/button";
+import {HoverCard, HoverCardContent, HoverCardTrigger} from "@/components/ui/hover-card";
 import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover";
 import {useSongsSingle} from "@/imports/api/songs/hooks";
 import {INITIAL_VOLUME} from "@/imports/common/config";
 import {SONG_FILE_PATH, VOCAL_FILE_PATH} from "@/imports/common/globals";
 import {Slider} from "@/imports/ui/customComponents/slider";
-import {Pause, Play, Volume, Volume1, Volume2, VolumeOff} from "lucide-react";
+import {CreativeCommons, Pause, Play, Volume, Volume1, Volume2, VolumeOff} from "lucide-react";
 import React, {useEffect, useRef, useState} from "react";
+import {useTranslation} from "react-i18next";
 import {useAudioContext} from "../contextProvider/AudioContext";
 import {cookies, getCookieSave} from "./Cookies";
 import {MarkerSlider} from "./MarkerSlider";
+import {P} from "./Typography";
+
+function AudioDetails({song}) {
+  const {t} = useTranslation();
+  const artistPage = song.artist.replace(/\s+/g, "_");
+  const url = `https://freemusicarchive.org/music/${artistPage}`;
+
+  return (
+    <HoverCardContent className="w-min">
+      <P className="whitespace-nowrap">
+        <b>{t("Collections.Songs.title")}:</b> {song.title}
+      </P>
+      <P className="whitespace-nowrap">
+        <b>{t("Collections.Songs.artist")}:</b> {song.artist}
+      </P>
+      <P className="whitespace-nowrap">
+        <b>{t("Collections.Songs.source")}:</b> Free Music Archive -{" "}
+        <a href={url} target="_blank" rel="noopener noreferrer" className="underline">
+          {song.artist}
+        </a>
+      </P>
+      <P className="whitespace-nowrap">
+        <b>{t("Collections.Songs.license")}:</b> {song.license}
+      </P>
+    </HoverCardContent>
+  );
+}
 
 export function AudioPlayer() {
   const {trackID, icon, isPlaying, setIsPlaying, useBackgroundMusic, jumpToFirstOnset} = useAudioContext();
@@ -108,9 +137,17 @@ export function AudioPlayer() {
         <div className="w-full md:max-h-22 md:h-22 sticky bottom-0 left-0 right-0 border-t-2 bg-background border-sidebar-border flex items-center md:p-2 px-2">
           <div className="w-full md:h-20 h-18 space-x-2 flex justify-center items-center">
             {icon && (
-              <div className="size-16 bg-accent rounded-md border-2 flex items-center justify-center">
-                <h1 className="text-center text-2xl font-bold">{icon}</h1>
-              </div>
+              <HoverCard openDelay={5} closeDelay={100}>
+                <HoverCardTrigger asChild>
+                  <div className="relative size-16">
+                    <CreativeCommons className="absolute -top-1 -right-1 bg-accent rounded-full" />
+                    <div className="size-16 bg-accent rounded-md border-2 flex items-center justify-center">
+                      <h1 className="text-center text-2xl font-bold">{icon}</h1>
+                    </div>
+                  </div>
+                </HoverCardTrigger>
+                <AudioDetails song={song} />
+              </HoverCard>
             )}
             <audio ref={audioRef} onTimeUpdate={handleTimeUpdate} onEnded={() => setIsPlaying(false)} />
 
