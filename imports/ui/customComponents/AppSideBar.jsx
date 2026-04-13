7fbd4f54f812c964a7bbb4ddd4061ed1ff4c6eb0
Christian Böndgen <c.boendgen@gmail.com>
2026-04-13 12:14:34 +0200

    add keyboard control infos

--- imports/ui/customComponents/AppSideBar.jsx
+++ imports/ui/customComponents/AppSideBar.jsx
@@ -1,3 +1,4 @@
+import {Kbd, KbdGroup} from "@/components/ui/kbd";
 import {
   Sidebar,
   SidebarContent,
@@ -6,6 +7,8 @@ import {
   SidebarGroupLabel,
   SidebarHeader,
   SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from "@/components/ui/sidebar";
 import React from "react";
 import {useTranslation} from "react-i18next";
@@ -15,6 +18,15 @@ import {LanguageToggle} from "./LanguageToggle";
 import {LoginButton} from "./LoginButton";
 import {NavigationArea} from "./NavigationArea";
 import {ThemeToggle} from "./ThemeToggle";
+
+function ControlsKey({name, children}) {
+  return (
+    <SidebarMenuItem className="grid grid-cols-3 gap-1 mx-1">
+      <div className="col-span-2 flex justify-start items-center">{name}</div>
+      <div className="col-span-1 flex justify-start items-center">{children}</div>
+    </SidebarMenuItem>
+  );
+}
 
 // ToDo: add translations for toggle buttons
 export function AppSideBar() {
@@ -42,6 +54,37 @@ export function AppSideBar() {
           </SidebarGroupContent>
         </SidebarGroup>
         <NavigationArea />
+        <SidebarGroup>
+          <SidebarGroupLabel>{t("Sidebar.controls")}</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <ControlsKey name={t("Sidebar.Controls.settings")}>
+                <Kbd>Esc</Kbd>
+              </ControlsKey>
+              <ControlsKey name={t("Sidebar.Controls.navigate")}>
+                <Kbd>{"<"}</Kbd>, <Kbd>{">"}</Kbd>
+              </ControlsKey>
+              <ControlsKey name={t("Sidebar.Controls.toggle")}>
+                <Kbd>T</Kbd>
+              </ControlsKey>
+              <ControlsKey name={t("Sidebar.Controls.submit")}>
+                <Kbd>↵</Kbd>
+              </ControlsKey>
+              <ControlsKey name={t("Sidebar.Controls.playPause")}>
+                <Kbd>␣</Kbd>
+              </ControlsKey>
+              <ControlsKey name={t("Sidebar.Controls.playX")}>
+                <Kbd>1</Kbd>, <Kbd>X</Kbd>
+              </ControlsKey>
+              <ControlsKey name={t("Sidebar.Controls.playA")}>
+                <Kbd>2</Kbd>, <Kbd>A</Kbd>
+              </ControlsKey>
+              <ControlsKey name={t("Sidebar.Controls.playB")}>
+                <Kbd>3</Kbd>, <Kbd>B</Kbd>
+              </ControlsKey>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
         <SidebarGroup>
           <LoginButton />
         </SidebarGroup>
