89643cbf32d517a250a9ed4eb6219974edf3187a
Christian Böndgen <c.boendgen@gmail.com>
2026-04-15 20:51:57 +0200

    filter for participants with 0 answers
    issue => #21

--- imports/ui/pages/participantList/ParticipantSearchForm.jsx
+++ imports/ui/pages/participantList/ParticipantSearchForm.jsx
@@ -12,6 +12,7 @@ import {AutoField} from "../../customComponents/AutoField";
 const searchFormSchema = z.object({
   _id: z.string().optional(),
   surveyCompleted: z.boolean().optional().default(false),
+  noQuestionsAnswered: z.boolean().optional().default(false),
   questionnaireID: z.number().optional(),
 });
 
@@ -22,6 +23,7 @@ export function ParticipantSearchForm({onFilterChange, query}) {
     defaultValues: {
       _id: query?._id || "",
       surveyCompleted: query?.surveyCompleted || false,
+      noQuestionsAnswered: query?.noQuestionsAnswered || false,
       questionnaireID: query?.questionnaireID || undefined,
     },
   });
@@ -58,6 +60,13 @@ export function ParticipantSearchForm({onFilterChange, query}) {
                 form={form}
                 name="surveyCompleted"
                 label={t("Collections.Participants.surveyCompleted")}
+                type="bool"
+              />
+              <AutoField
+                className="col-span-1"
+                form={form}
+                name="noQuestionsAnswered"
+                label={t("Collections.Participants.noQuestionsAnswered")}
                 type="bool"
               />
               <AutoField
