a615e8172df8a43dbc3d09c6a43e5fc003ca19ed
Christian Böndgen <c.boendgen@gmail.com>
2026-06-18 12:31:48 +0200

    add ranomized questionnaires

--- imports/api/surveyQuestions/helpers.js
+++ imports/api/surveyQuestions/helpers.js
@@ -1,6 +1,8 @@
-import {MIN_NUM_QUESTIONS_PER_SURVEY} from "@/imports/common/config";
+import {MIN_NUM_QUESTIONS_PER_SURVEY, NUM_QUESTIONS_PER_SURVEY} from "@/imports/common/config";
+import {Log} from "meteor/logging";
 import {Meteor} from "meteor/meteor";
 import {Participants} from "../participants/collection";
+import {Songs} from "../songs/collection";
 import {Questionnaires, SurveyQuestions} from "./collection";
 
 export async function getQuestionnaireIDAtomic() {
@@ -85,5 +87,60 @@ export async function toggleQuestionSkip({trackID, skipInSurvey}) {
     const questionsSkipped = await SurveyQuestions.countAsync({questionnaireID, skip: true});
 
     await Questionnaires.updateAsync({questionnaireID}, {$set: {skip, questionsSkipped}});
+  }
+}
+
+function generateRandomTriplet({currentTriplets, trackIDs, maxTries = 1000}) {
+  let randomTriplet;
+
+  for (let i = 0; i < maxTries; ++i) {
+    randomTriplet = new Set();
+    while (randomTriplet.size < 3) {
+      randomTriplet.add(trackIDs[Math.floor(Math.random() * trackIDs.length)]);
+    }
+    randomTriplet = [...randomTriplet];
+    const key = JSON.stringify(randomTriplet);
+    if (!currentTriplets.includes(key)) return randomTriplet;
+  }
+
+  Log.error("Could not generate a unique random triplet, using last tried version!");
+  return randomTriplet;
+}
+
+export async function generateRandomQuestionnaire({questionnaireID}) {
+  Log.debug(`Generating random questionnaire with ID ${questionnaireID}`);
+
+  // get available and usable trackIDs
+  const trackIDs = [
+    ...new Set((await Songs.find({skipInSurvey: false}, {fields: {trackID: 1}}).fetchAsync()).map((s) => s.trackID)),
+  ];
+
+  // get current questions as triplets
+  const currentTriplets = (await SurveyQuestions.find({skip: false}, {fields: {X: 1, A: 1, B: 1}}).fetchAsync()).map((q) =>
+    JSON.stringify([q.X, q.A, q.B]),
+  );
+
+  await Questionnaires.insertAsync({
+    questionnaireID,
+    participantCount: 0,
+    questionsSkipped: 0,
+    skip: false,
+    isRandomized: true,
+  });
+
+  for (let questionNumber = 0; questionNumber < NUM_QUESTIONS_PER_SURVEY; ++questionNumber) {
+    const randomTriplet = generateRandomTriplet({currentTriplets, trackIDs});
+    const [X, A, B] = randomTriplet;
+
+    await SurveyQuestions.insertAsync({
+      questionnaireID,
+      questionNumber,
+      X,
+      A,
+      B,
+      skip: false,
+    });
+
+    currentTriplets.push(JSON.stringify(randomTriplet));
   }
 }
